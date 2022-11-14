8e71c438160df412fccabf4d7df3df657b0bd517
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-11-13 23:46:19 -0500

    CHORE: add logger middleware

--- v1/back/server.js
+++ v1/back/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const logger = require('./middleware/logger');
 const products = require('./routes/products');
 
 const app = express();
+
+app.use(logger);
 
 //Router for products
 app.use('/api/v1/products', products);
