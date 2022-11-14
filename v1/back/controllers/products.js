7aa5d7bd4b51c4479c62f9ac8dd9df0423164dc6
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-11-14 18:35:04 -0500

    CHORE: controller and service for Products

--- v1/back/controllers/products.js
+++ v1/back/controllers/products.js
@@ -1,37 +1,80 @@
+const ProductsService = require("../service/products");
+const service = new ProductsService();
+
 // @desc    Get all Products
 // @route   GET /api/v1/products
+// @route   GET /api/v1/products?size=10&offset=10
 // @access  Public 
 exports.getProducts = (req, res, next) => {
+  try{
+    const { size, offset } = req.query;
+    const limit = size;
+    const products = service.findAll(limit, offset);
     res
       .status(200)
-      .json({ success: true, msg: 'Show all products'});
+      .json({ success: true, result: products});
+  } catch (error){
+    res
+    .status(404)
+    .json({ message: error.message });
   }
-  
+}
 // @desc    Get One Product
 // @route   GET /api/v1/products/:id
 // @access  Public 
 exports.getProduct = (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = service.findOne(id);
+    res.json(product);
+  } catch (error) {
     res
-      .status(200)
-      .json({ success: true, msg: `Display Product with id: ${req.params.id}` });
+      .status(404)
+      .json({ message: error.message });
   }
+}
 
 // @desc    Create One Product
-// @route   POST /api/v1/products/:id
+// @route   POST /api/v1/products
 // @access  Public 
-exports.createProduct=(req, res, next)=>{
-    res.status(200).json({ 
+exports.createProduct=  (req, res, next)=>{
+  try {
+    const body = req.body;
+    console.log("req: ", Object.keys(req.params));
+    console.log("body: ", body);
+    const newProduct = service.create(body);
+
+    res.status(201).json({
       success: true, 
-      msg: 'Create new product'});
+      result: newProduct });
+  } catch (error) {
+    res
+    .status(404)
+    .json({ message: error.message })
   }
+
+
+};
 
 // @desc    Update Product
 // @route   PUT /api/v1/products
 // @access  Public
-  exports.updateProduct = (req, res, next)=>{
-    res
-      .status(200)
-      .json({ success: true, msg: `Update Product with id: ${req.params.id}`});
+  exports.updateProduct = async (req, res, next)=>{
+
+    try {
+      let body = req.body;
+      console.log()
+      console.log("req.BODY from updateProduct: ", req.body);
+      product = service.update(req.params.id, req.body);
+
+      res.status(200).json({
+        success: true, 
+        result: product });
+    } catch (error) {
+      res
+      .status(404)
+      .json({ message: error.message })
+    }
   }
   
   
@@ -39,7 +82,18 @@ exports.createProduct=(req, res, next)=>{
 // @route   DELETE /api/v1/products
 // @access  Public
   exports.deleteProduct = (req, res, next)=>{
-    res
-      .status(200)
-      .json({ success: true, msg: `Delete Product with id: ${req.params.id}`});
+
+    try {
+      const product = service.delete(req.params.id);
+      res
+        .status(200)
+        .json({success: true, result: product});
+    } catch (error) {
+      res
+      .status(404)
+      .json({ message: error.message })
+    }
+
+
+;
   }
---
+++ v1/back/service/products.js
@@ -0,0 +1,66 @@
+const mysqlConnection = require('../config/database');
+
+class ProductsService {
+
+    constructor(){
+      this.products = [];
+      this.generate();
+    }
+  
+    generate() {
+        mysqlConnection.query('SELECT * FROM product ORDER BY id ASC', (error, results) => {
+        if (error) {
+          throw error
+        }
+        this.products = results;
+      })
+    }
+  
+    create(data) {
+      console.log("data: ", data);
+      const newProduct = {
+        id: this.products.slice(-1)[0].id + 1,
+        ...data
+      }
+      this.products.push(newProduct);
+      console.log("newProduct: ", newProduct);
+      return newProduct;
+    }
+  
+    findAll(limit, offset) {
+      return limit && offset ? this.products.slice(offset, limit) : this.products;
+    }
+  
+    findOne(id) {
+      const product = this.products.find(item => item.id == id);
+      if (product == undefined) {
+        throw new Error('Product not found');
+      }
+      return product;
+    }
+  
+    update(id, changes) {
+      const index = this.products.findIndex(item => item.id == id);
+      if (index === -1) {
+        throw new Error('Product not found');
+      }
+      const product = this.products[index];
+      this.products[index] = {
+        ...product,
+        ...changes
+      };
+      return this.products[index];
+    }
+  
+    delete(id) {
+      const index = this.products.findIndex(item => item.id == id);
+      if (index === -1) {
+        throw new Error('Product not found');
+      }
+      this.products.splice(index, 1);
+      return { id };
+    }
+  
+  }
+  
+  module.exports = ProductsService;
